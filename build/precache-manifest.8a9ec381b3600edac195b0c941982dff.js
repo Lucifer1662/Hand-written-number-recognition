d314d2ecc186ddbc9c3372c7d7a842b68867c150
l.hawk99@hotmail.com <l.hawk99@hotmail.com>
2020-10-11 15:13:34 +1100

    added build

---
+++ build/precache-manifest.8a9ec381b3600edac195b0c941982dff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d123417800041a777994b7d8404214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a091fcfcdcedf6e2ae16",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10e8aa5f370b348a3d79",
+    "url": "/static/js/2.8e0b4ec4.chunk.js"
+  },
+  {
+    "revision": "79a642b0fa4a32c9bc89e8d26b6825af",
+    "url": "/static/js/2.8e0b4ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a091fcfcdcedf6e2ae16",
+    "url": "/static/js/main.fb786f68.chunk.js"
+  },
+  {
+    "revision": "7019f06d1933ed4e6a82",
+    "url": "/static/js/runtime-main.207056cf.js"
+  }
+]);
