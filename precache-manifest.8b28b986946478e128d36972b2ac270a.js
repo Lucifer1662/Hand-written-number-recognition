41058771d5801de09dbd6f4d5f029ee1c3330fc9
l.hawk99@hotmail.com <l.hawk99@hotmail.com>
2020-10-11 15:29:22 +1100

    Updates

---
+++ precache-manifest.8b28b986946478e128d36972b2ac270a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7a9821d1c4d6a26612838dea5d447b",
+    "url": "/Hand-written-number-recognition/index.html"
+  },
+  {
+    "revision": "a586f365369d30f6bd26",
+    "url": "/Hand-written-number-recognition/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "10e8aa5f370b348a3d79",
+    "url": "/Hand-written-number-recognition/static/js/2.8e0b4ec4.chunk.js"
+  },
+  {
+    "revision": "79a642b0fa4a32c9bc89e8d26b6825af",
+    "url": "/Hand-written-number-recognition/static/js/2.8e0b4ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a586f365369d30f6bd26",
+    "url": "/Hand-written-number-recognition/static/js/main.c750cd4e.chunk.js"
+  },
+  {
+    "revision": "2938a81fbc1637e9bdd9",
+    "url": "/Hand-written-number-recognition/static/js/runtime-main.8c62073b.js"
+  }
+]);
